bd90b2da58834b141b98c261ce129b90fdca131b
kazuyaseki <track613armory@gmail.com>
2020-04-06 19:09:56 +0900

    Fix not showing dialog

--- src/code.tsx
+++ src/code.tsx
@@ -1,7 +1,7 @@
 import { subscribeOnMessages } from 'react-figma';
 
-figma.showUI(__html__, { visible: false });
+figma.showUI(__html__);
 
-figma.ui.onmessage = message => {
+figma.ui.onmessage = (message) => {
   subscribeOnMessages(message);
 };
